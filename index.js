a1b8a735ab00869b0ead0825f0c0493bf6819047
axle12693 <axle12693@gmail.com>
2019-06-25 22:18:38 -0600

    start forum project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser')
+const { Pool } = require("pg");
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: connectionString});
 
