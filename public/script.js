33eefbb0b9c3f2cb4ad56b89b89dd5fd7b0005a4
axle12693 <axle12693@gmail.com>
2019-06-27 11:27:12 -0600

    fix self stupidity again

--- public/script.js
+++ public/script.js
@@ -1,13 +1,19 @@
 function displayForumCategories()
 {
     $.get("/forum/forum_categories", function(data, status){
-        json_fcats = JSON.parse(data);
+        json_fcats = data;//JSON.parse(data);
         html = "<div class=\"container\">";
-        json_fcats.foreach(function(element) {
+        // json_fcats.foreach(function(element) {
+        //     html += "<div class=\"card bg-primary text-white\">";
+        //     html += "<div class=\"card-header\"><h4>" + element["title"] + "</h4></div>";
+        //     html += "</div>";
+        // });
+        for (var key in json_fcats)
+        {
             html += "<div class=\"card bg-primary text-white\">";
-            html += "<div class=\"card-header\"><h4>" + element["title"] + "</h4></div>";
+            html += "<div class=\"card-header\"><h4>" + json_fcats[key] + "</h4></div>";
             html += "</div>";
-        });
+        }
         html += "</div>";
       });
 }
