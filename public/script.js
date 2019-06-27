94f2cdb7b75ad227780e544c3eb530e5dcf38db1
axle12693 <axle12693@gmail.com>
2019-06-27 11:10:02 -0600

    Get rid of await

--- public/script.js
+++ public/script.js
@@ -6,11 +6,6 @@ function displayForumCategories()
         json_fcats.foreach(function(element) {
             html += "<div class=\"card bg-primary text-white\">";
             html += "<div class=\"card-header\"><h4>" + element["title"] + "</h4></div>";
-            await $.get("/forum/forums", function(data2, statu2s) {
-                json_forums = JSON.parse(data2);
-                json_forums.foreach(function(element2) {
-                    html += "<div class=\"card-body alink\">" + element2["title"] + "</div>"
-                });
             html += "</div>";
             });
         });
