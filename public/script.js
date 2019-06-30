20b14050c2a03d89ed67dbe3d11fdd7caa70ae02
axle12693 <axle12693@gmail.com>
2019-06-29 20:30:22 -0600

    Display forums

--- public/script.js
+++ public/script.js
@@ -1,20 +1,29 @@
 function displayForumCategories()
 {
-    $.get("/forum/forum_categories", function(data, status){
-        json_fcats = data;//JSON.parse(data);
-        html = "<div class=\"container\">";
-        // json_fcats.foreach(function(element) {
-        //     html += "<div class=\"card bg-primary text-white\">";
-        //     html += "<div class=\"card-header\"><h4>" + element["title"] + "</h4></div>";
-        //     html += "</div>";
-        // });
+    $.get("/forum/forum_categories", function(json_fcats, status){
+        let html = "<div class=\"container\">";
         for (var key in json_fcats)
         {
-            html += "<div class=\"card bg-primary text-white\">";
+            html += "<div class=\"card bg-primary text-white\" id=\"fcat" + json_fcats[key]["forum_category_id"] + "\">";
             html += "<div class=\"card-header\"><h4>" + json_fcats[key]["title"] + "</h4></div>";
             html += "</div>";
         }
         html += "</div>";
         $("#body_container").html(html);
+        for (var key in json_fcats)
+        {
+            displayForums(json_fcats[key]["forum_category_id"]);
+        }
       });
+}
+
+function displayForums(cat_id)
+{
+    $.get("/forum/forumsInCategory/" + cat_id, function(data, status){
+        let fcat_el = $("#fcat" + cat_id);
+        for (var key in data)
+        {
+            fcat_el.html(fcat.html() + "<div class=\"card-body alink\">" + data[key]["title"]);
+        }
+    });
 }
