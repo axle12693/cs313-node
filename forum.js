1ee925024da820918c37134d9e43ee73da375618
axle12693 <axle12693@gmail.com>
2019-07-05 15:29:43 -0600

    fix stupidity

--- forum.js
+++ forum.js
@@ -21,7 +21,7 @@ exports.forum_setup = app => {
       });
     })
     .get("/forum/postsInForum/:id", function(req, res) {
-      sql = "SELECT * FROM Post WHERE forum__id = $1";
+      sql = "SELECT * FROM Post WHERE forum_id = $1";
       pool.query(sql, [req.params.id], function(err, result) {
         if (err) {
           console.log("Error in query: ")
